9e24ceee0d4b62520bb6c996b60ec8f6ab7237da
bigdatakey <44523332+bigdatakey@users.noreply.github.com>
2019-09-01 15:41:34 +0300

    Add files via upload

--- resources/views/pages/statuspage.blade.php
+++ resources/views/pages/statuspage.blade.php
@@ -1,1 +1,16 @@
+<?php //var_dump($statusData) ?>
+@extends('layouts.master')
 
+@section('content')
+<div class="main">
+    <div class="breadcrumbs">
+        <div class="cont">
+            <a href="/">Главная</a>&#8194;/&#8194;<span>Статус заказа № {{ $statusData['id'] }}</span>
+        </div>
+    </div>
+	<newstatus :statusdata="{{ $statusData }}"></newstatus>
+</div>
+
+
+{{-- 4387356198 --}}
+@endsection
